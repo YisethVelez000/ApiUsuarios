e440f0fec29f6dac48ea49d620d86be2b7ad4783
Alison Puerta <131219053+YisethVelez000@users.noreply.github.com>
2023-12-10 14:05:37 -0500

    Añadimos cors

--- routes/auth.js
+++ routes/auth.js
@@ -10,6 +10,7 @@ const bcrypt = require('bcrypt');
 // validation
 const Joi = require('@hapi/joi');
 
+
 const schemaRegister = Joi.object({
     name: Joi.string().min(6).max(255).required(),
     email: Joi.string().min(6).max(255).required().email(),
@@ -22,6 +23,9 @@ const schemaLogin = Joi.object({
     email: Joi.string().min(6).max(255).required().email(),
     password: Joi.string().min(6).max(1024).required()
 })
+//usamos cors para poder hacer peticiones desde el front
+router.use(cors());
+
 
 router.post('/register', async (req, res) => {
 
