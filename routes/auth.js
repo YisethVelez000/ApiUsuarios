ad8ab65ce866b29041c9c623b9782b3fa5205167
Alison Puerta <131219053+YisethVelez000@users.noreply.github.com>
2024-03-12 07:07:12 -0500

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -17,7 +17,6 @@ const schemaRegister = Joi.object({
     email: Joi.string().min(6).max(255).required().email(),
     password: Joi.string().min(6).max(1024).required(),
     rol: Joi.string().required(),
-    precioDolar: Joi.number().required()
 })
 
 const schemaLogin = Joi.object({
